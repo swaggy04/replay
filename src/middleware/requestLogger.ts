c1aea8b50c05a86657b9fad27408677bdab93bf9
swagatpadhi <litan3104@gmail.com>
2026-08-23 20:25:22 +0530

    chore:added middleware to record data

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,13 +1,17 @@
 import type { Request, Response, NextFunction } from "express";
 import prisma from "../lib/prisma.js";
 
-export async function requestLogger(req: Request, _res: Response, next: NextFunction) {
+export async function requestLogger(req: Request, res: Response, next: NextFunction) {
   try {
     await prisma.requestLog.create({
       data: {
         method: req.method,
         path: req.path,
       },
+    });
+    const startTime = Date.now();
+    res.on("finish", () => {
+      // We'll fill this in next.
     });
     console.log(`Recorded ${req.method} ${req.path}`);
     next();
