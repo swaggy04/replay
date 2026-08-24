fbffa3622e27832c69f4048f5e6993f4a4071092
swagatpadhi <litan3104@gmail.com>
2026-08-24 20:36:59 +0530

    updated reqlogger

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
+import "dotenv/config";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
