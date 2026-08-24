e42d22d6dfb54ae833de869953ac04c8efaaae51
swagatpadhi <litan3104@gmail.com>
2026-08-24 23:49:20 +0530

    updated schema

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -4,17 +4,29 @@ import prisma from "../lib/prisma.js";
 export async function requestLogger(req: Request, res: Response, next: NextFunction) {
   const startTime = Date.now();
   try {
-    await prisma.requestLog.create({
+   const requestLog =  await prisma.requestLog.create({
       data: {
         method: req.method,
         path: req.path,
         body: req.body,
       },
+      
     });
+   
 
-    res.on("finish", () => {
-      const duration = Date.now() - startTime;
+    res.on("finish", async() => {
+      const durationMs = Date.now() - startTime;
       console.log(`${req.method} ${req.path} → ${res.statusCode} (${duration}ms)`);
+      await prisma.requestLog.update({
+        where: {
+          id: requestLog.id
+        },
+        data: {
+          
+          statusCode: res.statusCode,
+          durationMs: durationMs,
+        }
+      })
     });
     console.log(`Recorded ${req.method} ${req.path}`);
     next();
