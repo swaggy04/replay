61e6da9464b22c113457fbaacac90dcfd505c9e5
swagatpadhi <litan3104@gmail.com>
2026-08-30 16:41:02 +0530

    added try/catch

--- src/controllers/replayController.ts
+++ src/controllers/replayController.ts
@@ -1,15 +1,20 @@
 import type { Request, Response } from "express";
+
 import { replayService } from "../services/replayService.js";
+
 import prisma from "../lib/prisma.js";
 
 export async function replayController(req: Request, res: Response) {
   const requestId = req.params.id;
+
   if (typeof requestId !== "string") {
     return res.status(400).json({
       message: "Invalid request ID",
     });
   }
+
   const replay = await replayService(requestId);
+
   if (!replay) {
     return res.status(404).json({
       message: "Request not found",
@@ -24,35 +29,46 @@ export async function replayController(req: Request, res: Response) {
   delete headers["content-length"];
   delete headers.connection;
   delete headers["accept-encoding"];
-  const startTime = Date.now();
 
-  const response = await fetch(replay.url, {
-    method: replay.method,
-    headers,
-    body: replay.body,
-  });
+  try {
+    const startTime = Date.now();
 
-  const contentType = response.headers.get("content-type");
+    const response = await fetch(replay.url, {
+      method: replay.method,
+      headers,
+      body: replay.body,
+    });
 
-  let responseBody;
+    const contentType = response.headers.get("content-type");
 
-  if (contentType?.includes("application/json")) {
-    responseBody = await response.json();
-  } else {
-    responseBody = await response.text();
+    let responseBody;
+
+    if (contentType?.includes("application/json")) {
+      responseBody = await response.json();
+    } else {
+      responseBody = await response.text();
+    }
+
+    const durationMs = Date.now() - startTime;
+
+    await prisma.replayExecution.create({
+      data: {
+        requestLogId: requestId,
+        statusCode: response.status,
+        responseBody,
+        durationMs,
+      },
+    });
+
+    return res.status(response.status).json({
+      status: response.status,
+      body: responseBody,
+    });
+  } catch (error) {
+    console.error("Replay failed:", error);
+
+    return res.status(500).json({
+      message: "Replay failed",
+    });
   }
-  const durationMs = Date.now() - startTime;
-  await prisma.replayExecution.create({
-    data: {
-      requestLogId: requestId,
-      statusCode: response.status,
-      responseBody,
-      durationMs,
-    },
-  });
-  return res.status(response.status).json({
-    status: response.status,
-    body: responseBody,
-  });
 }
-
