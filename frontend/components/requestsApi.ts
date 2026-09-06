84fce6a966f2aee31c72b201dffeabd8400f16ee
swagatpadhi <litan3104@gmail.com>
2026-09-06 18:36:27 +0530

    refactoring the tabs

--- frontend/components/requestsApi.ts
+++ frontend/components/requestsApi.ts
@@ -1,9 +1,5 @@
-
-
 export async function getRequests(page: number) {
-  const response = await fetch(
-    `http://localhost:5000/requests?page=${page}&limit=10`
-  );
+  const response = await fetch(`http://localhost:5000/requests?page=${page}&limit=10`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch requests");
@@ -12,12 +8,25 @@ export async function getRequests(page: number) {
   return response.json();
 }
 
-export async function getRequestDetails(id:string){
- const response = await fetch(`http://localhost:5000/requests/${id}`);
- if (!response.ok) {
+export async function getRequestDetails(id: string) {
+  const response = await fetch(`http://localhost:5000/requests/${id}`);
+  if (!response.ok) {
     throw new Error("Failed to fetch requests");
   }
 
   return response.json();
+}
 
+export async function replayRequest(id: string) {
+  const response = await fetch(`http://localhost:5000/replay/${id}`, {
+    method: "POST",
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Replay request failed");
+  }
+
+  return data;
 }
